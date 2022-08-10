954935d035ab04d8c96adf5b4a62bf62c425c151
androdox <niko.andrade.p@gmail.com>
2022-08-10 01:44:53 -0500

    medio

--- App.py
+++ App.py
@@ -1,3 +1,4 @@
+from tkinter.messagebox import NO
 from urllib import request
 import cx_Oracle
 from flask import Flask, render_template, request, redirect, url_for, flash
@@ -13,6 +14,8 @@ connection = None
 datosauxiliar = []
 fecha = ''
 programa = []
+docente = []
+elemento = []
 try:
     connection = cx_Oracle.connect(
         config.username,
@@ -98,10 +101,8 @@ def auxiliar():
             data = cursor.fetchall()
             global fecha
             fecha = fechatemp[0]
-            print(fecha)
             global datosauxiliar
             datosauxiliar = data[0]
-            print(datosauxiliar)
             connection.commit()
             return render_template('registroauxiliar.html', datos=data[0], fecha=fecha)
         else:
@@ -112,6 +113,10 @@ def auxiliar():
 @app.route('/docente', methods=['GET'])
 def docente():
     if request.method == 'GET':
+        global docente
+        global fecha
+        global programa
+        global elemento
         nombre = request.args.get('nom')
         apellido = request.args.get('apel')
         sql = """SELECT  e.nomempleado nombre, e.apellempleado nombre, c.descargo , s.nomespacio, e.codempleado
@@ -122,28 +127,68 @@ def docente():
         cursor.execute(sql, [nombre, apellido])
         data = cursor.fetchall()
         if (len(data) > 0):
+            
             docente = data[0]
-            sqlpro = """select r.fechafin, r.fechaini, a.descactividad, d.nomdeporte, s.nomespacio, p.cupo 
-            from responsable r, programacion p, actividad a, deporte d, espacio s 
+            sqlpro = """select r.fechafin, r.fechaini, a.descactividad, d.nomdeporte, s.nomespacio, p.cupo, CASE
+            WHEN r.fechafin >= sysdate AND r.fechaini <= sysdate THEN 'SI'
+            Else 'NO'
+            END AS tiempo, s.codespacio, p.CONSECPROGRA, r.CONSECRES
+            from responsable r, programacion p, actividad a, deporte d, espacio s
             where r.codempleado = :empleado and r.consecprogra = p.consecprogra 
             and a.idactividad = p.idactividad and d.iddeporte = p.iddeporte and s.codespacio = p.codespacio"""
             cursor = connection.cursor()
             empleado = data[0][4]
             cursor.execute(sqlpro, [empleado])
             data = cursor.fetchall()
-            print(data)
             if (len(data) > 0):
                 programa = data[0]
+                if (str(data[0][6]) == 'SI'):
+                    sql = """select ed.consecelemento, te.desctipoelemento from elemendeportivo ed,  tipoelemento te 
+                    where ed.idtipoelemento= te.idtipoelemento and ed.idestado = \'1\' and ed.codespacio = :espacio"""
+                    cursor.execute(sql, [data[0][7]])
+                    elemento = cursor.fetchall()
+                    sql = """INSERT INTO asistirresponsable (CONSECPROGRA, CONSECRES, FECHAASISRES, HORAASISRES)
+                    VALUES (:programa, :responsable, sysdate, sysdate)"""
+                    cursor.execute(sql, [data[0][8], data[0][9]])
+                else:
+                    elemento = None
             else:
                 programa = None
+                elemento = None
 
         else:
             docente = None
             programa = None
+            elemento = None
         connection.commit()
         global datosauxiliar
-        return render_template('registroauxiliar.html', datos=datosauxiliar, docente=docente, fecha=fecha, programa=programa)
+        return render_template('registroauxiliar.html', datos=datosauxiliar, docente=docente, fecha=fecha, programa=programa, elemento=elemento)
 
+@app.route('/asignar/<dato>')
+def asignar(dato):
+    global docente
+    global fecha
+    global programa
+    global elemento
+    global datosauxiliar
+    print(dato)
+    cursor = connection.cursor()
+    sql = """ select a.consecasisres from asistirresponsable a where a.CONSECPROGRA = :programa and a.CONSECRES = :responsable order by a.fechaasisres desc"""
+    cursor.execute(sql, [programa[8], programa[9]])
+    datos = cursor.fetchall()
+    id = datos[0]
+    sql = """INSERT INTO prestamo (CONSECPROGRA, CONSECRES, CONSECASISRES, CONSECELEMENTO) 
+    VALUES (:programa, :responsabel, :id, :dato) """
+    cursor.execute(sql, [programa[8], programa[9], id[0], dato])
+    sql = """update elemendeportivo set idestado=\'2\' where  CONSECELEMENTO = :dato"""
+    cursor.execute(sql,[dato])
+    sql = """select ed.consecelemento, te.desctipoelemento from elemendeportivo ed,  tipoelemento te 
+    where ed.idtipoelemento= te.idtipoelemento and ed.idestado = \'1\' and ed.codespacio = :espacio"""
+                    
+    cursor.execute(sql, [programa[7]])
+    elemento = cursor.fetchall()
+    connection.commit()
+    return render_template('registroauxiliar.html', datos=datosauxiliar, docente=docente, fecha=fecha, programa=programa, elemento=elemento)
 
 @app.route('/eliminar/<idempleado>')
 def eliminarEmpleado(idempleado):
